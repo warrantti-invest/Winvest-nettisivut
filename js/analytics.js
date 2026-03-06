328c88122641d17fa6117b2e9903a1a40e4db9fb
Ville Tenhunen <villetenhunen@Ville-MacBook-Air.local>
2026-03-06 03:03:19 +0200

    Lisätty js-kansio

---
+++ js/analytics.js
@@ -0,0 +1,7 @@
+(function() {
+  // Load Vercel Analytics script
+  var script = document.createElement('script');
+  script.src = '/_vercel/insights/script.js';
+  script.defer = true;
+  document.head.appendChild(script);
+})();
